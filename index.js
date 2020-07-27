2fb2d742c8a4c75182040c3b18aaa4283656fba8
NED-FYP <57455687+NED-FYP@users.noreply.github.com>
2020-07-27 22:55:23 +0500

    Procfile updated

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express();
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
-app.listen(8000, () => {
-  console.log('Example app listening on port 8000!')
-});
